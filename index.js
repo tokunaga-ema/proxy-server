b60279a22e87b07467b84674711dda877392fedd
KOSAIDO\tokunaga.ema <tokunaga.ema@kosaido.co.jp>
2022-12-09 14:09:41 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const { createProxyMiddleware } = require("http-proxy-middleware")
+const rateLimit = require("express-rate-limit")
+require("dotenv").config()
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10000,
+})
+
+app.get("/",(req, res) => {
+  res.send("This is my proxy server")
+})
+
+app.use("/corona-tracker-world-data", limiter,(req, res, next) => {
+  createProxyMiddleware({
+    target: process.env.BASE_API_URL_CORONA_WORLD,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^/corona-tracker-world-data`]: "",
+    },
+  })(req, res, next)
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Listening on localhost port ${port}`)
+})
+
+module.exports = app
